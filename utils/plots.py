8107b7ee7bfd342ca667c52b5769f0fc9fe29c23
Setiawanfikri <95399474+Setiawanfikri@users.noreply.github.com>
2022-04-18 11:16:00 +0700

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -88,9 +88,8 @@ def plot_wh_methods():  # from utils.general import *; plot_wh_methods()
 
 def output_to_target(output, width, height):
     # Convert model output to target format [batch_id, class_id, x, y, w, h, conf]
-   if isinstance(output, torch.Tensor):
+    if isinstance(output, torch.Tensor):
         output = output.cpu().numpy()
-
     targets = []
     for i, o in enumerate(output):
         if o is not None:
@@ -99,11 +98,12 @@ def output_to_target(output, width, height):
                 box = pred[:4]
                 w = (box[2] - box[0]) / width
                 h = (box[3] - box[1]) / height
-@@ -104,7 +104,6 @@ def output_to_target(output, width, height):
+                x = box[0] / width + w / 2
+                y = box[1] / height + h / 2
+                conf = pred[4]
                 cls = int(pred[5])
 
                 targets.append([i, cls, x, y, w, h, conf])
-
     return np.array(targets)
 
 
